713e4d6750e49aa5280f22ed8346abf29322f545
DaryaNov <dasha_novoselova98@mail.ru>
2020-07-23 02:20:55 +0600

    Коммит артикл и миграцию

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+
+    description = models.CharField(max_length=2000, null=False, blank=False, verbose_name='Описание')
+    status = models.TextField(max_length=30, null=True, blank=False, default='new',verbose_name='Статус')
+    date_completion = models.DateField(max_length=10, null=True,blank=False, verbose_name='Дата выполнения')
+
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.status)
+
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
