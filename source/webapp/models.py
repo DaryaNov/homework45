9c5997be1f4f9a98a8072bc7d6243f4919a983e8
DaryaNov <dasha_novoselova98@mail.ru>
2020-07-23 02:57:42 +0600

    Коммит добавили статусы

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 
+STATUS_CHOICES = [
+    ('new', 'Новая'),
+    ('in_progress', 'В процессе'),
+    ('done', 'Сделано')
+]
+
+
+
 class Article(models.Model):
 
     description = models.CharField(max_length=2000, null=False, blank=False, verbose_name='Описание')
-    status = models.TextField(max_length=30, null=True, blank=False, default='new',verbose_name='Статус')
+    status = models.TextField(max_length=30,choices=STATUS_CHOICES, default='new',verbose_name='Статус')
     date_completion = models.DateField(max_length=10, null=True,blank=False, verbose_name='Дата выполнения')
 
 
