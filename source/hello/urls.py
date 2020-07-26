c1c95b6193bda21bf3f453f7d5c722d159ee28d1
DaryaNov <dasha_novoselova98@mail.ru>
2020-07-26 16:22:16 +0600

    commit add index urls

--- source/hello/urls.py
+++ source/hello/urls.py
@@ -17,14 +17,16 @@ from django.contrib import admin
 
 from django.urls import path
 
-from webapp.views import index_view, article_view
+from webapp.views import index_view, article_view, article_create
 
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('', index_view),
-    path('articles/add/', article_view)
+    path('', index_view, name='index'),
+    path('article/<int:pk>/', article_create, name='article_create'),
+    path('articles/add/', article_view, name='article_view')
 
 ]
+
 
